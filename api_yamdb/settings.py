68b7755c51bcd1c469500ee84f20a3e53c121d31
Ruslan Khachin <ruslan.13.sv@gmail.com>
2021-07-01 20:18:34 +0300

    я устал

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = os.getenv('SECRET_KEY', default='very_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
